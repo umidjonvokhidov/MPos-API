61b802227982afd8e08dac523ddebd7eaef7044e
umidjonvokhidov <umidjonvokhidov.uz@gmail.com>
2025-07-01 16:07:02 +0500

    cookie secured

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET || "your-session-secret",
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false }, // Set to true if using HTTPS
+  cookie: { secure: true }, // Set to true if using HTTPS
 }));
 app.use(passport.initialize());
 app.use(passport.session());
