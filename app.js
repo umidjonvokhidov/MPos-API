c5e6683968314a3e4b802b522dec548b61b51728
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-07-09 19:05:22 +0500

    Update CORS

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 app.use(
   helmet({
     contentSecurityPolicy:
