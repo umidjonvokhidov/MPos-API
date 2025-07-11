354f024b415367ea48ee640a7231cb099b0cda44
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-07-11 09:31:33 +0500

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -132,36 +132,6 @@ export const refreshTokenRoute = async (req, res, next) => {
   }
 };
 
-export const authorize = async (req, res, next) => {
-  try {
-    let token;
-
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-
-    if (!token) {
-      const error = new Error("Unauthorized");
-      error.statusCode = 401;
-      throw error;
-    }
-
-    const decoded = jwt.verify(token, JWT_SECRET);
-
-    const user = await User.findById(decoded.id);
-
-    if (!user) return res.status(401).json({ message: "Unauthorized" });
-
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).json({ message: "Unauthorized", error: error.message });
-    next(error);
-  }
-};
 
 
 export const SignOut = (req, res, next) => {
