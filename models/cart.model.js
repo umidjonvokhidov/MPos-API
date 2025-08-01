0cb28946e7bc232462ecbfbbcc45579e1f6ff70c
umidjonvokhidov <umidjonvokhidov.uz@gmail.com>
2025-08-01 20:16:02 +0500

    update cart model

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,26 +1,37 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: [true, "User is required"],
-  },
-  products: [
-    {
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: [true, "Products are required"],
-      },
-      count: {
-        type: Number,
-        required: [true, "Product count is required"],
-        min: [1, "Count must be at least 1"],
-      },
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
     },
-  ],
-});
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: [true, "Products are required"],
+        },
+        count: {
+          type: Number,
+          required: [true, "Product count is required"],
+          min: [1, "Count must be at least 1"],
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
 
 cartSchema.virtual("totalPrice").get(function () {
   if (!this.products || this.products.length === 0) return 0;
