68e4ced9e47ba3072827b47f16779be9c14609af
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-08-17 19:26:52 +0500

    Update app.js

--- app.js
+++ app.js
@@ -51,14 +51,7 @@ app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "uploads"), {
-    setHeaders: (res, path) => {
-      res.set("Access-Control-Allow-Origin", "*");
-    },
-  })
-);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // const limiter = rateLimit({
 //   windowMs: 15 * 60 * 1000,
